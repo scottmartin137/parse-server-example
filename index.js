f995151886d171075541a7c6078f7ff668a93b64
scottmartin137 <scottmartin137@gmail.com>
2017-07-03 20:08:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  
-  
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
